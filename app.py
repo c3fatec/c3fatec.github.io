a783eb528d578e9a86e484796ff90d11129dc56b
Gabriel Santos <dosantos.ogabriel@gmail.com>
2022-09-13 09:52:32 -0300

    começar projeto

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
 
-@app.route("/")
-def hello_world():
-    return render_template("main.html")
+@app.route("/home")
+def home():
+    return render_template("principal.html")
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
